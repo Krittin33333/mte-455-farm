b1fe1bc66822fc9670054a93bdc9ebfd903d2b35
Krittin33333 <aomgg555@gmail.com>
2023-07-15 13:47:20 +0700

    Office code

--- Assets/Script/Manager/GameManager.cs
+++ Assets/Script/Manager/GameManager.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public static GameManager instance;
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] private int day = 0;
+    public int Day { get { return day; } set { day = value; } }
+
+    private void Awake()
     {
-        
+        instance = this;
     }
 }
